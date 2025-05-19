ddb53a14ac8c4e6592a6cd07a8865da49bb9a53a
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2025-05-19 08:00:29 +0000

    initial commit

---
+++ article/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import index, show, create, edit, update, delete
+
+app_name = 'article'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('<int:pk>/', show, name='show'),
+    path('create/', create, name='create'),
+    path('<int:pk>/edit/', edit, name='edit'),
+    path('<int:pk>/update/', update, name='update'),
+    path('<int:pk>/delete/', delete, name='delete'),
+]
+
+
